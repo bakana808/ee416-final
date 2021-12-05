60a6f501ea00aea3aa2c6df1ec22c5a9f27c7c2c
Branden Akana <branden.akana@gmail.com>
2021-12-04 20:00:16 -1000

    implement dataset organization and splitting (Chase)

--- README.md
+++ README.md
@@ -3,18 +3,20 @@
 
 A convolutional neural network that classifies a chest CT image of various patients as having COVID-19 pneumonia, other pneumonia, or being healthy.
 
-## Dataset
+## Overview
+
+### Dataset
 -   ~4,400 2D lung CT images
 -   3 classes/labels
     -   COVID-19 pneumonia
     -   other pneumonia
     -   healthy condition
 
-## Test & Training Data
+### Test & Training Data
 -   split 80% training and 20% test using 5-fold cross validation
 -   (optional) data augmentation by rotating images 90, 180, and 270 degrees to quadruple the amount of images
 
-## Tasks
+### Tasks
 1.  create a program that splits the dataset into training and test data and automatically forms a folder structure
     -   80% training, 20% test using 5-fold cross validation
 2.  finish the convolutional neural network (CNN) by implementing the 2nd layer convolution kernels, convlution operator, and summation
@@ -32,6 +34,12 @@ A convolutional neural network that classifies a chest CT image of various patie
     -   graphs of training and test loss across epochs (iterations)
     -   accuracy of best model on the test set
 
-## Notes:
+### Notes:
 FCN: fully-connected network
 CNN: convolutional neural network
+
+## Running
+Before running the program, place the folder of images labeled "COVID" in `Data/Covid`, the folder of images labeled "Healthy" in `Data/Healthy`, and the folder of images labeled "Other pnumonia" in `Data/Others`. The program will automatically move and rename these files.
+
+Then, you can run:
+`python eg.py`
--- eg.py
+++ eg.py
@@ -1,4 +1,8 @@
 #%%
+import re
+
+import os
+import random
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,6 +19,22 @@ from dataset import DataSet
 from cnn import Network
 
 # ================================================================================
+# PROGRAM PARAMETERS
+# ================================================================================
+
+# path to covid images
+DATASET_COVID_PATH = "Data/Covid"
+
+# path to healthy images
+DATASET_HEALTHY_PATH = "Data/Healthy"
+
+# path to "other pnumonia" images
+DATASET_OTHER_PATH = "Data/Others"
+
+# path to output all images to
+DATASET_OUTPUT = "Dataset"
+
+# ================================================================================
 # TRAINING PARAMETERS
 # ================================================================================
 
@@ -26,7 +46,7 @@ NUM_EPOCHS = 100
 # OPTIMIZER PARAMETERS
 # ================================================================================
 
-# learning rate
+# learning rate; the higher this number is, the faster the weights adjust when training
 LR = 0.001
 
 # epsilon; the term added to the denominator to improve numerical stability
@@ -35,13 +55,64 @@ EPS = 1e-8
 # penalty
 WEIGHT_DECAY = 0
 
-
-# ================================================================================
-# DATASET ORGANIZATION & SPLITTING
+# %% DATASET ORGANIZATION
 # ================================================================================
 
-# Load the dataset and train and test splits
-print("Loading datasets...")
+
+def get_label(path):
+    """Get the label from the name of a sample's filepath."""
+
+    r = re.compile("([a-z]+)([0-9])+")
+    m = r.match(path)
+    label = m.group(1)
+    return label
+
+
+def consolidate_data(dir, new_dir, label, req_ext=".png"):
+    """Move all the files (recursively) in dir to new_dir.
+
+    The files will be prefixed with `label` and suffixed
+    with an incrementing number starting at 0.
+
+    Returns the number of files moved.
+    """
+    print(f"Moving files from {dir} to {new_dir} (labeled '{label}')...")
+
+    num_files = 0
+    for root, _, files in os.walk(dir):
+        for filename in files:
+            ext = os.path.splitext(filename)[1]  # file extension
+            path = root + os.sep + filename
+            new_path = f"{new_dir}\\{label}{num_files}{ext}"
+            if ext == req_ext:
+                os.replace(path, new_path)
+                num_files += 1
+
+    print(f"Moved {num_files} files.")
+    return num_files
+
+
+# perform dataset consolidation
+moved_samples = 0
+moved_samples += consolidate_data(DATASET_COVID_PATH, DATASET_OUTPUT, "covid")
+moved_samples += consolidate_data(DATASET_HEALTHY_PATH, DATASET_OUTPUT, "healthy")
+moved_samples += consolidate_data(DATASET_OTHER_PATH, DATASET_OUTPUT, "other")
+
+print(f"Moved a total of {moved_samples} files.")
+
+# %% DATASET SPLITTING
+# ================================================================================
+
+# list of samples (images)
+samples = os.listdir(DATASET_OUTPUT)
+print(f"Total samples = {len(samples)}")
+
+random.shuffle(samples)  # randomize order of samples
+
+folds = np.array_split(samples, 5)
+
+# %% DATASET PREPROCESSING
+# ================================================================================
 
 # Data path
 DATA_train_path = Dataset("./Dataset/Train")
@@ -70,7 +141,7 @@ testloader = DataLoader(Data_test, batch_size=BATCH_SIZE, shuffle=True)
 
 ## declaration of Network
 
-# MODEL SETUP
+# %% MODEL SETUP
 # ================================================================================
 
 # the hardware device (gpu or cpu) to use when training
